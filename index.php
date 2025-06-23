c60d4df5c43f750d008d6d49e7988fe4c125f17d
Galvancillo <angelgalvan82@gmail.com>
2025-06-23 17:47:25 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/Estilos.css">
+    <title>Sneakers World</title>
+    <h1 class="titulo">Bienvenido a Sneakers World</h1>
+   
+    
+    
+</head>
+<body>
+    <nav class="navbar">
+      <ul class="nav-menu">
+        <li><a href="#">Lo Nuevo</a></li>
+        <li><a href="#">Hombre</a></li>
+        <li><a href="#">Mujer</a></li>
+        <li><a href="#">Niño/a</a></li>
+        <li><a href="#">Estilos</a></li>
+      </ul>
+         <div class="nav-right">
+            <div class="search-box">
+                <span class="icon">🔍</span>
+                 <input type="text" placeholder="Buscar">
+      </div>
+
+        <span class="icon">🛒</span>
+    </div>
+
+    </nav>
+</body>
+</html>
